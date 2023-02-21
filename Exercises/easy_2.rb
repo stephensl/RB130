4acfec8f8e97cc51fd04d88d76e6b9a089c41879
Lawton Stephens <stephens.lawton@gmail.com>
2023-02-21 15:18:33 -0500

    easy_2 (ip)

--- Exercises/easy_2.rb
+++ Exercises/easy_2.rb
@@ -154,5 +154,162 @@
 #-------------------------------------------------------------------------------
 #                                 QUESTION: 5
 #-------------------------------------------------------------------------------
+# drop_while
+
+# The Enumerable#drop_while method begins by iterating over the members of a collection, passing each element to the associated block until it finds an element for which the block returns false or nil. It then converts the remaining elements of the collection (including the element that resulted in a falsey return) to an Array, and returns the result.
+
+# Write a method called drop_while that behaves similarly for Arrays. It should take an Array as an argument, and a block. It should return all the elements of the Array, except those elements at the beginning of the Array that produce a truthy value when passed to the block.
+
+# If the Array is empty, or if the block returns a truthy value for every element, drop_while should return an empty Array.
+
+# Your method may use #each, #each_with_object, #each_with_index, #inject, loop, for, while, or until to iterate through the Array passed in as an argument, but must not use any other methods that iterate through an Array or any other collection.
+
+# Examples:
+
+# drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+# drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+# drop_while([1, 3, 5, 6]) { |value| true } == []
+# drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+# drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+# drop_while([]) { |value| true } == []
 
 
+#-------------------------------------------------------------------------------
+#                                 ANSWER: 5
+#-------------------------------------------------------------------------------
+# def drop_while(collection)
+#   collection.each_with_index do |element, idx|
+#     unless yield(element) then return collection[idx..-1]
+#     end 
+#   end 
+#   []
+# end 
+
+#===============================================================================
+#===============================================================================
+#===============================================================================
+
+
+#-------------------------------------------------------------------------------
+#                                 QUESTION: 6
+#-------------------------------------------------------------------------------
+# each_with_index
+
+# The Enumerable#each_with_index method iterates over the members of a collection, passing each element and its index to the associated block. The value returned by the block is not used. each_with_index returns a reference to the original collection.
+
+# Write a method called each_with_index that behaves similarly for Arrays. It should take an Array as an argument, and a block. It should yield each element and an index number to the block. each_with_index should return a reference to the original Array.
+
+# Your method may use #each, #each_with_object, #inject, loop, for, while, or until to iterate through the Array passed in as an argument, but must not use any other methods that iterate through an Array or any other collection.
+
+# Examples:
+
+# result = each_with_index([1, 3, 6]) do |value, index|
+#   puts "#{index} -> #{value**index}"
+# end
+
+# puts result == [1, 3, 6]
+
+# 0 -> 1
+# 1 -> 3
+# 2 -> 36
+# true
+
+#-------------------------------------------------------------------------------
+#                                 ANSWER: 6
+#-------------------------------------------------------------------------------
+def each_with_index(collection)
+  current_index = 0 
+
+  collection.each do |element|
+    yield(element, current_index)
+    current_index += 1
+  end 
+end   # utilizing return value from each method which is original collection.
+
+
+
+#===============================================================================
+#===============================================================================
+#===============================================================================
+
+
+#-------------------------------------------------------------------------------
+#                                 QUESTION: 7
+#-------------------------------------------------------------------------------
+# each_with_object
+
+# The Enumerable#each_with_object method iterates over the members of a collection, passing each element and an arbitrary object (usually a collection) to the associated block. The value returned by the block is not used. The object that is passed to the block is defined as an argument to each_with_object; with each iteration, this object may be updated by the block. When iteration is complete, the final value of the object is returned.
+
+# Write a method called each_with_object that behaves similarly for Arrays. It should take an Array and an object of some sort as an argument, and a block. It should yield each element and the object to the block. each_with_object should return the final value of the object.
+
+# If the Array is empty, each_with_object should return the original object passed as its second argument.
+
+# Your method may use #each, #each_with_index, #inject, loop, for, while, or until to iterate through the Array passed in as an argument, but must not use any other methods that iterate through an Array or any other collection.
+
+# Examples:
+
+# result = each_with_object([1, 3, 5], []) do |value, list|
+#   list << value**2
+# end
+# result == [1, 9, 25]
+
+# result = each_with_object([1, 3, 5], []) do |value, list|
+#   list << (1..value).to_a
+# end
+# result == [[1], [1, 2, 3], [1, 2, 3, 4, 5]]
+
+# result = each_with_object([1, 3, 5], {}) do |value, hash|
+#   hash[value] = value**2
+# end
+# result == { 1 => 1, 3 => 9, 5 => 25 }
+
+# result = each_with_object([], {}) do |value, hash|
+#   hash[value] = value * 2
+# end
+# result == {}
+
+#-------------------------------------------------------------------------------
+#                                 ANSWER: 7
+#-------------------------------------------------------------------------------
+def each_with_object(array, object)
+  array.each do |element|
+    yield(element, object)
+  end 
+  object 
+end 
+
+#===============================================================================
+#===============================================================================
+#===============================================================================
+
+
+#-------------------------------------------------------------------------------
+#                                 QUESTION: 8
+#-------------------------------------------------------------------------------
+# max_by
+
+# The Enumerable#max_by method iterates over the members of a collection, passing each element to the associated block. It then returns the element for which the block returned the largest value.
+
+# Write a method called max_by that behaves similarly for Arrays. It should take an Array and a block, and return the element that contains the largest value.
+
+# If the Array is empty, max_by should return nil.
+
+# Your method may use #each, #each_with_object, #each_with_index, #inject, loop, for, while, or until to iterate through the Array passed in as an argument, but must not use any other methods that iterate through an Array or any other collection.
+
+# Examples:
+
+# max_by([1, 5, 3]) { |value| value + 2 } == 5
+# max_by([1, 5, 3]) { |value| 9 - value } == 1
+# max_by([1, 5, 3]) { |value| (96 - value).chr } == 1
+# max_by([[1, 2], [3, 4, 5], [6]]) { |value| value.size } == [3, 4, 5]
+# max_by([-7]) { |value| value * 3 } == -7
+# max_by([]) { |value| value + 5 } == nil
+
+#-------------------------------------------------------------------------------
+#                                 ANSWER: 8
+#-------------------------------------------------------------------------------
+
+
+#===============================================================================
+#===============================================================================
+#===============================================================================

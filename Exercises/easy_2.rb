106d48a80e94addecbe590fb5d0dae85237a5676
Lawton Stephens <stephens.lawton@gmail.com>
2023-02-22 08:54:47 -0500

    easy_2

--- Exercises/easy_2.rb
+++ Exercises/easy_2.rb
@@ -217,14 +217,14 @@
 #-------------------------------------------------------------------------------
 #                                 ANSWER: 6
 #-------------------------------------------------------------------------------
-def each_with_index(collection)
-  current_index = 0 
+# def each_with_index(collection)
+#   current_index = 0 
 
-  collection.each do |element|
-    yield(element, current_index)
-    current_index += 1
-  end 
-end   # utilizing return value from each method which is original collection.
+#   collection.each do |element|
+#     yield(element, current_index)
+#     current_index += 1
+#   end 
+# end   # utilizing return value from each method which is original collection.
 
 
 
@@ -271,12 +271,12 @@ end   # utilizing return value from each method which is original collection.
 #-------------------------------------------------------------------------------
 #                                 ANSWER: 7
 #-------------------------------------------------------------------------------
-def each_with_object(array, object)
-  array.each do |element|
-    yield(element, object)
-  end 
-  object 
-end 
+# def each_with_object(array, object)
+#   array.each do |element|
+#     yield(element, object)
+#   end 
+#   object 
+# end 
 
 #===============================================================================
 #===============================================================================
@@ -308,6 +308,145 @@ end
 #-------------------------------------------------------------------------------
 #                                 ANSWER: 8
 #-------------------------------------------------------------------------------
+
+# def max_by(array)
+#   return nil if array.empty?
+
+#   current_max_element = array.first 
+#   current_max_value = yield(current_max_element)
+
+#   (1..array.size - 1).each do |idx|
+#     current_element = array[idx]
+#     current_value = yield(current_element)
+
+#     if current_value > current_max_value 
+#       current_max_element, current_max_value = current_element, current_value 
+#     end 
+#   end 
+
+#   current_max_element 
+# end 
+
+#===============================================================================
+#===============================================================================
+#===============================================================================
+
+#-------------------------------------------------------------------------------
+#                                 QUESTION: 9
+#-------------------------------------------------------------------------------
+# each_cons (Part 1)
+
+# The Enumerable#each_cons method iterates over the members of a collection taking each sequence of n consecutive elements at a time and passing them to the associated block for processing. It then returns a value of nil.
+
+# Write a method called each_cons that behaves similarly for Arrays, taking the elements 2 at a time. The method should take an Array as an argument, and a block. It should yield each consecutive pair of elements to the block, and return nil.
+
+# Your method may use #each, #each_with_object, #each_with_index, #inject, loop, for, while, or until to iterate through the Array passed in as an argument, but must not use any other methods that iterate through an Array or any other collection.
+
+# Examples:
+
+# hash = {}
+# result = each_cons([1, 3, 6, 10]) do |value1, value2|
+#   hash[value1] = value2
+# end
+# result == nil
+# hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+# hash = {}
+# result = each_cons([]) do |value1, value2|
+#   hash[value1] = value2
+# end
+# hash == {}
+# result == nil
+
+# hash = {}
+# result = each_cons(['a', 'b']) do |value1, value2|
+#   hash[value1] = value2
+# end
+# hash == {'a' => 'b'}
+# result == nil
+
+#-------------------------------------------------------------------------------
+#                                 ANSWER: 9
+#-------------------------------------------------------------------------------
+# def each_cons(collection)
+#   first_index = 0
+#   second_index = 1 
+
+#   while second_index <= collection.size - 1 do 
+#     yield(collection[first_index], collection[second_index])
+#     first_index += 1
+#     second_index += 1
+#   end 
+# end 
+
+# # or 
+
+# def each_cons(array)
+#   array.each_with_index do |item, index|
+#     break if index + 1 >= array.size 
+#     yield(item, array[index + 1])
+#   end 
+#   nil 
+# end 
+
+#===============================================================================
+#===============================================================================
+#===============================================================================
+
+
+#-------------------------------------------------------------------------------
+#                                 QUESTION: 10
+#-------------------------------------------------------------------------------
+# each_cons (Part 2)
+
+# In the previous exercise, we wrote a method that emulates Enumerable#each_cons, but limited our method to handling 2 elements at a time. Enumerable#each_cons can actually handle any number of elements at a time: 1, 2, 3, or more.
+
+# Update your each_cons method so it takes an argument that specifies how many elements should be processed at a time.
+
+# Your method may use #each, #each_index, #each_with_object, #inject, loop, for, while, or until to iterate through the Array passed in as an argument, but must not use any other methods that iterate through an Array or any other collection.
+
+# Examples:
+# hash = {}
+# each_cons([1, 3, 6, 10], 1) do |value|
+#   hash[value] = true
+# end
+# hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+
+# hash = {}
+# each_cons([1, 3, 6, 10], 2) do |value1, value2|
+#   hash[value1] = value2
+# end
+# hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+# hash = {}
+# each_cons([1, 3, 6, 10], 3) do |value1, *values|
+#   hash[value1] = values
+# end
+# hash == { 1 => [3, 6], 3 => [6, 10] }
+
+# hash = {}
+# each_cons([1, 3, 6, 10], 4) do |value1, *values|
+#   hash[value1] = values
+# end
+# hash == { 1 => [3, 6, 10] }
+
+# hash = {}
+# each_cons([1, 3, 6, 10], 5) do |value1, *values|
+#   hash[value1] = values
+# end
+# hash == {}
+
+
+#-------------------------------------------------------------------------------
+#                                 ANSWER: 10
+#-------------------------------------------------------------------------------
+
+# def each_cons(collection, number)
+#   0.upto(collection.size - number) do |start_index|
+#     yield(*collection[start_index, number]) 
+#   end 
+#   nil 
+# end
 
 
 #===============================================================================

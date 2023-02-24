c2e01fdfffa9ae49610f6e37960aa225898617c4
Lawton Stephens <stephens.lawton@gmail.com>
2023-02-24 06:56:33 -0500

    easy_testing

--- Exercises/easy_testing.rb
+++ Exercises/easy_testing.rb
@@ -53,3 +53,64 @@ assert_nil(value)
 
 ---
 ---
+
+# Question: 
+
+# Empty Object Assertions
+
+# Write a minitest assertion that will fail if the Array list is not empty.
+
+#
+
+#
+
+# Answer: 
+ 
+assert_empty(list)  # preferred
+
+# or 
+
+assert_equal(true, list.empty?)
+
+---
+---
+
+# Question 
+
+# Included Object Assertions
+
+# Write a minitest assertion that will fail if the 'xyz' is not in the Array list.
+
+
+#
+#
+
+# Answer: 
+
+assert_include(list, 'xyz')   # assert_include: first arg contains second arg
+
+# or 
+
+assert_equal(true, list.include?('xyz'))
+
+---
+---
+
+# Question: 
+
+# Exception Assertions
+
+# Write a minitest assertion that will fail unless employee.hire raises a NoExperienceError exception.
+
+#
+#
+
+# Answer: 
+
+assert_raises(NoExperienceError) { employee.hire } 
+
+# does block raise exception of the named type. 
+
+---
+---
+

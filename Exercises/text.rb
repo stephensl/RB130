e9641bcfb4a534eb16034930fe671f7f111ad1e9
Lawton Stephens <stephens.lawton@gmail.com>
2023-03-08 13:25:52 -0500

    complete medium_2

--- Exercises/text.rb
+++ Exercises/text.rb
@@ -1,10 +1,21 @@
-class Text 
+class Text
   def initialize(text)
-    @text = text 
-  end 
+    @text = text
+  end
 
   def swap(letter_one, letter_two)
     @text.gsub(letter_one, letter_two)
-  end 
-end 
+  end
 
+  def word_count
+    @text.split.count
+  end
+end
+
+words = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed vulputate ipsum.
+Suspendisse commodo sem arcu. Donec a nisi elit. Nullam eget nisi commodo, volutpat
+quam a, viverra mauris. Nunc viverra sed massa a condimentum. Suspendisse ornare justo
+nulla, sit amet mollis eros sollicitudin et. Etiam maximus molestie eros, sit amet dictum dolor ornare bibendum. Morbi ut massa nec lorem tincidunt elementum vitae id magna. Cras et varius mauris, at pharetra mi."
+text = Text.new(words)
+
+p text.word_count
--- Exercises/text_test.rb
+++ Exercises/text_test.rb
@@ -1,30 +1,5 @@
 # # Testing for text.rb 
 
-# require 'minitest/autorun'
-# require_relative 'text'
-
-# class TextTest < Minitest::Test 
-#   def setup 
-#     @file = File.open('./sample_text.txt', 'r')
-#   end 
-
-#   def test_swap 
-#     text = Text.new(@file.read)
-#     actual_text = text.swap('a', 'e')
-#     expected_text = <<~TEXT.chomp
-#     Lorem ipsum dolor sit emet, consectetur edipiscing elit. Cres sed vulputete ipsum.
-#     Suspendisse commodo sem ercu. Donec e nisi elit. Nullem eget nisi commodo, volutpet
-#     quem e, viverre meuris. Nunc viverre sed messe e condimentum. Suspendisse ornere justo nulle, sit emet mollis eros sollicitudin et. Etiem meximus molestie eros, sit emet dictum dolor ornere bibendum. Morbi ut messe nec lorem tincidunt elementum vitee id megne. Cres et verius meuris, et pheretre mi.
-#     TEXT
-
-#     assert_equal(expected_text, actual_text)
-#   end
-
-#   def teardown 
-#     @file.close
-#   end 
-# end
-
 require 'minitest/autorun'
 require_relative 'text'
 
@@ -48,4 +23,8 @@ class TextTest < Minitest::Test
   def teardown
     @file.close
   end
+
+  def test_word_count 
+    assert_equal(72, @text.word_count)
+  end 
 end
